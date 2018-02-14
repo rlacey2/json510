5e198d7742485ccb52e3e785c7f80f396e99d67d
rlacey2@gmail.com <rlacey2@gmail.com>
2018-02-14 18:49:45 +0000

    fixes

--- _ngClient/js/controllers/v1.js
+++ _ngClient/js/controllers/v1.js
@@ -11,15 +11,12 @@ angularnodeApp.controller('V1Ctrl', ['$scope' ,  'jsonModels',
 			var total = 0;
 			var count = 0;
 			var len = data2.length;
-			for(var i = 0; i < len; i++){
-				 
-				 
+			for(var i = 0; i < len; i++){				  
 				total +=  data2[i].mark  ; // total = total + data2.mark
 				count++;
 			}
 			return { "result" : (total / count).toFixed(2), "count" : count} ;
 		}
-
 	}]); // V1Ctrl
 	
  
